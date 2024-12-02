eabb4aa6b6de8e116624b18c6c2a904ab245a003
Alex Miranda <alexmirandamoraes@gmail.com>
2024-12-02 20:51:07 +0100

    quest-01 part 3

--- quest-01/main.zig
+++ quest-01/main.zig
@@ -1,8 +1,5 @@
 const std = @import("std");
-const print = std.debug.print;
 const panic = std.debug.panic;
-const assert = std.debug.assert;
-const testing = std.testing;
 const expectEqual = std.testing.expectEqual;
 
 pub fn main() !void {
@@ -11,49 +8,30 @@ pub fn main() !void {
     defer bw.flush() catch {}; // don't forget to flush!
     const stdout = bw.writer();
 
-    var gpa = std.heap.GeneralPurposeAllocator(.{ .safety = true }){};
-    defer if (gpa.deinit() == .leak) panic("Memory leak.", .{});
-    var ally = gpa.allocator();
+    var buf: [9000]u8 = undefined;
+    for (1..4) |i| {
+        var path_buf: [21]u8 = undefined;
+        const path = try std.fmt.bufPrint(&path_buf, "quest-01/notes-p{d}.txt", .{i});
+        const notes = std.fs.cwd().openFile(path, .{}) catch |err| {
+            switch (err) {
+                error.FileNotFound => panic("Notes file is missing", .{}),
+                else => panic("Failed to open notes: {any}", .{err}),
+            }
+        };
+        defer notes.close();
 
-    const notes_p1 = std.fs.cwd().openFile("quest-01/notes-p1.txt", .{}) catch |err| {
-        switch (err) {
-            error.FileNotFound => panic("Notes file is missing", .{}),
-            else => panic("Failed to open notes: {any}", .{err}),
-        }
-    };
-
-    const data_p1 = try notes_p1.readToEndAlloc(ally, 1000);
-    defer ally.free(data_p1);
-    const answer_p1 = preparePotions(data_p1);
-    try stdout.print("Part 1: {d}\n", .{answer_p1});
-
-    const notes_p2 = std.fs.cwd().openFile("quest-01/notes-p2.txt", .{}) catch |err| {
-        switch (err) {
-            error.FileNotFound => panic("Notes file is missing", .{}),
-            else => panic("Failed to open notes: {any}", .{err}),
-        }
-    };
-
-    const data_p2 = try notes_p2.readToEndAlloc(ally, 2000);
-    print("{s} ({d})\n", .{ data_p2, data_p2.len });
-    defer ally.free(data_p2);
-    const answer_p2 = preparePotionsPairs(data_p2);
-    try stdout.print("Part 2: {d}\n", .{answer_p2});
+        const read = try notes.readAll(&buf);
+        const answer = preparePotions(buf[0..read], i);
+        try stdout.print("Part {d}: {d}\n", .{ i, answer });
+    }
 }
 
-fn preparePotions(s: []const u8) usize {
+fn preparePotions(s: []const u8, chunk: usize) usize {
     var count: usize = 0;
-    for (s) |c| count += potionsNeeded(c, 0);
-    return count;
-}
-
-fn preparePotionsPairs(s: []const u8) usize {
-    var count: usize = 0;
-    var it = std.mem.window(u8, s, 2, 2);
-    while (it.next()) |pair| {
-        const alone = (pair[0] == 'x' or pair[1] == 'x');
-        count += potionsNeeded(pair[0], if (alone) 0 else 1);
-        count += potionsNeeded(pair[1], if (alone) 0 else 1);
+    var it = std.mem.window(u8, s, chunk, chunk);
+    while (it.next()) |group| {
+        const extra = chunk - std.mem.count(u8, group, "x") -| 1;
+        for (group) |c| count += potionsNeeded(c, extra);
     }
     return count;
 }
@@ -69,13 +47,13 @@ fn potionsNeeded(creature: u8, extra: usize) usize {
 }
 
 test "part 1" {
-    try expectEqual(5, preparePotions("ABBAC"));
+    try expectEqual(5, preparePotions("ABBAC", 1));
 }
 
 test "part 2" {
-    try expectEqual(28, preparePotionsPairs("AxBCDDCAxD"));
+    try expectEqual(28, preparePotions("AxBCDDCAxD", 2));
 }
 
 test "part 3" {
-    return error.SkipZigTest;
+    try expectEqual(30, preparePotions("xBxAAABCDxCC", 3));
 }
